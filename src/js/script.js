a16353622cd6e70d4762d5d50ee8ad0ac8f83b07
Gabriel Ferreira <gabrielmoraes2507@outlook.com>
2023-12-29 16:29:44 -0300

    changed colors

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,10 @@ const email = document.getElementById("email");
 const formControl = document.querySelector(".form-control");
 const successMessage = document.querySelector(".success-message");
 const container = document.querySelector(".container");
-const emailDigitado = document.getElementById("email-digitado");
+const emailProvided = document.getElementById("email-provided");
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  emailValue = email.value;
+  const emailValue = email.value;
   if (emailValue === "") {
     formControl.classList.add("error");
   } else if (!validateEmail(emailValue)) {
@@ -14,7 +14,7 @@ button.addEventListener("click", (e) => {
   } else {
     container.style.display = "none";
     successMessage.classList.add("success");
-    emailDigitado.innerText = emailValue;
+    emailProvided.innerText = emailValue;
   }
 });
 function validateEmail(email) {
